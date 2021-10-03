cb4965745a8abee30a61f839279e7d411758e410
biminkoju <biminkoju@gmail.com>
2021-10-03 15:52:27 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let i, j, k;
+i = "string";
+j = 10;
+k = true;
+const a = [2, 3, 4, 5];
+function v() {
+    console.log(`${i}, ${j}, ${k}`);
+    console.log(`${a[3]}`);
+}
+if (a[2] > 2) console.log(`three is greater than 2`);
+v();

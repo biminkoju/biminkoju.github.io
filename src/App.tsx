64498fc4ed8172da348df683518f836d0e2e0a8f
biminkoju <biminkoju@gmail.com>
2024-09-21 02:18:30 +0545

    react using vite

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+// import { Button } from './components/ui/button';
+
+function App() {
+	return (
+		<>
+			<h1 className="text-3xl font-bold underline">Hello world!</h1>
+			<h2>The site is under development</h2>
+			<img
+				src="./src/assets/img/firstdayatwork.png"
+				height={500}
+				width={500}
+			></img>
+		</>
+	);
+}
+
+export default App;
